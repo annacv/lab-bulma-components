a4211c4cb271ea42a6b3c649a17a3edadfd3e96d
annacv <21217131+annacv@users.noreply.github.com>
2019-08-05 18:24:38 +0200

    Form component

--- lab-bulma-react/src/App.js
+++ lab-bulma-react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
+import FormField from './components/FormField.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FormField />
     </div>
   );
 }
