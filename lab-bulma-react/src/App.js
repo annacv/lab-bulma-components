57623baaeb4d2520a3b3a8b6e7f6bf5caf8a1bc2
annacv <21217131+annacv@users.noreply.github.com>
2019-08-05 23:45:00 +0200

    [R] Add responsive columns

--- lab-bulma-react/src/App.js
+++ lab-bulma-react/src/App.js
@@ -8,13 +8,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div class="container">
-        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-        <FormField label="Email" type="email" placeholder="e.g alexsmith@gmail.com" />
-      </div>
-      <CoolButton className="button is-rounded my-class is-danger is-small" value="Button 1" />
-      <CoolButton className="button is-small is-success" value="Button 2" />
+      <Navbar text="Home" />
+      <section className="section">
+        <div className="container">
+          <div className="column is-half-tablet is-two-fifths-desktop is-offset-one-quarter-desktop">
+            <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+            <FormField label="Email" type="email" placeholder="e.g alexsmith@gmail.com" />
+          </div>
+          <div className="column is-two-fifths is-offset-one-quarter-desktop">
+            <CoolButton className="button is-rounded my-class is-danger is-small" value="Button 1" />
+            <CoolButton className="button is-small is-success" value="Button 2" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
