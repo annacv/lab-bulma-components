63e1db864a20a2958f146ecf4dff2672722f8613
annacv <21217131+annacv@users.noreply.github.com>
2019-08-05 23:43:58 +0200

    [RF] Refactor nav to include button element

--- lab-bulma-react/src/components/Navbar.js
+++ lab-bulma-react/src/components/Navbar.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import CoolButton from './CoolButton.js'
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-              <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"></img>
+      <nav className="navbar is-transparent" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"></img>
+          </a>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="https://bulma.io/">
+              {this.props.text}
             </a>
           </div>
-
-          <div class="navbar-start">
-            <a class="navbar-item" href="https://bulma.io/">
-              Home
-              </a>
-          </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="field is-grouped">
-                <p class="control">
-                  <a class="button is-info" href="/">
-                    <span>Login</span>
-                  </a>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <CoolButton className="button is-info" value="Login" />
                 </p>
-                <p class="control">
-                  <a class="button is-primary" href="/">
-                    <span>Signup</span>
-                  </a>
+                <p className="control">
+                  <CoolButton className="button is-primary" value="Signup" />
                 </p>
               </div>
             </div>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     )
   }
 }
 
-export default Navbar;
+export default Navbar
